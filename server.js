8c0499bedcae32ca2881d678dbc06d0ec8a60dcf
Akuma9923 <ninjastylegmg@gmail.com>
2024-06-13 15:49:41 +0300

    git commit -m "Added file upload functionality"

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const path = require('path');
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
+// Servire fișiere statice din directorul 'public'
 app.use(express.static('public'));
 
+// Endpoint pentru încărcarea fișierelor
 app.post('/upload', upload.array('files[]'), (req, res) => {
     res.send('Files uploaded successfully.');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Endpoint pentru servirea 'index.html' la rădăcină
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Rulează serverul
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
